711075c631e49c2f57e43eb0088e5732559a7889
wise-vg <17162714046@163.com>
2019-06-28 22:20:05 +0800

    update

---
+++ js/scroll-reveal.js
@@ -0,0 +1,6 @@
+ScrollReveal().reveal('.board-a', { duration: 500, distance: '100px', origin: 'right', easing: 'ease-out' });
+ScrollReveal().reveal('.board-b', { duration: 1000, distance: '1000px', origin: 'right', easing: 'ease-out' });
+ScrollReveal().reveal('.board-c', { duration: 1500, distance: '2000px', origin: 'right', easing: 'ease-out' });
+ScrollReveal().reveal('.feature-box', { interval: 200, distance: '100px', origin: 'bottom', easing: 'ease-out' });
+ScrollReveal().reveal('.team-member', { interval: 200, distance: '100px', origin: 'bottom', easing: 'ease-out' });
+ScrollReveal().reveal('.token-suply', {  interval: 200, distance: '800px', origin: 'right', easing: 'ease-out' });
